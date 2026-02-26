00d24625dec5ddff0c8879c172e5341c11fde4bb
Rheymann14 <67354994+Rheymann14@users.noreply.github.com>
2026-02-26 09:11:29 +0800

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,31 @@
 
         <title inertia>{{ config('app.name', 'CHED') }}</title>
 
+        @php
+  
+            $meta = $page['props']['meta'] ?? [];
+
+            $ogTitle = $meta['title'] ?? config('app.name', 'CMSP');
+            $ogDescription = $meta['description'] ?? 'CHED Merit Scholarship Program.';
+            $ogImage = $meta['image'] ?? asset('ched_logo.png'); // <-- put your default thumbnail here
+            $ogUrl = $meta['url'] ?? url()->current();
+        @endphp
+
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="{{ e($ogTitle) }}">
+        <meta property="og:description" content="{{ e($ogDescription) }}">
+        <meta property="og:url" content="{{ $ogUrl }}">
+
+        <meta property="og:image" content="{{ $ogImage }}">
+        <meta property="og:image:secure_url" content="{{ $ogImage }}">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{ e($ogTitle) }}">
+        <meta name="twitter:description" content="{{ e($ogDescription) }}">
+        <meta name="twitter:image" content="{{ $ogImage }}">
+
         <link rel="icon" href="/ched_logo.png" sizes="any">
         <!-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> -->
         <link rel="apple-touch-icon" href="/ched_logo.png">
